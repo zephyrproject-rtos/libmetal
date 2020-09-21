c8ffc84c71f63f7d292515d578135006e8c70465
Eugene Cohen <eugene@nuviainc.com>
2020-09-21 13:08:22 -0600

    zephyr: connect metal log system to zephyr log system
    Use log_generic instead of printk so metal logging is included
    with zephyr logging.
    
    Signed-off-by: Eugene Cohen <eugene@nuviainc.com>

--- libmetal/lib/system/zephyr/log.c
+++ libmetal/lib/system/zephyr/log.c
@@ -13,28 +13,42 @@
 #include <metal/log.h>
 #include <zephyr.h>
 
-static const char * const level_strs[] = {
-	"metal: emergency: ",
-	"metal: alert:     ",
-	"metal: critical:  ",
-	"metal: error:     ",
-	"metal: warning:   ",
-	"metal: notice:    ",
-	"metal: info:      ",
-	"metal: debug:     ",
+#if defined(CONFIG_LOG)
+#include <logging/log.h>
+LOG_MODULE_REGISTER(metal);
+#endif
+
+static uint8_t metal_to_zephyr_log_level[] = {
+	LOG_LEVEL_ERR,		/* METAL_LOG_EMERGENCY */
+	LOG_LEVEL_ERR,		/* METAL_LOG_ALERT */
+	LOG_LEVEL_ERR,		/* METAL_LOG_CRITICAL */
+	LOG_LEVEL_ERR,		/* METAL_LOG_ERROR */
+	LOG_LEVEL_WRN,		/* METAL_LOG_WARNING */
+	LOG_LEVEL_INF,		/* METAL_LOG_NOTICE */
+	LOG_LEVEL_INF,		/* METAL_LOG_INFO, */
+	LOG_LEVEL_DBG		/* METAL_LOG_DEBUG */
 };
 
 void metal_zephyr_log_handler(enum metal_log_level level,
 			      const char *format, ...)
 {
 	va_list args;
+	struct log_msg_ids src_level = {
+		.level = metal_to_zephyr_log_level[level],
+		.domain_id = CONFIG_LOG_DOMAIN_ID,
+		.source_id = LOG_CURRENT_MODULE_ID()
+	};
 
-	if (level <= METAL_LOG_EMERGENCY || level > METAL_LOG_DEBUG)
+	if (level <= METAL_LOG_EMERGENCY || level > METAL_LOG_DEBUG) {
 		level = METAL_LOG_EMERGENCY;
-	printk("%s", level_strs[level]);
+	}
 
 	va_start(args, format);
-	vprintk(format, args);
+
+#if defined(CONFIG_LOG)
+	log_generic(src_level, format, args);
+#endif
+
 	va_end(args);
 }
 
