d6bedace0dea9e32d6b97af47928a13a6203e7e5
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-10-11 10:39:51 +0200

    Add support for hosted test environments
    Add a new hosted test environment "processor".
    
    This is meant as a build target used when the
    code is not built for a real target
    but as part of a test in a hosted environment
    (for ex. as a test environment like Zephyr's
    native_sim target).
    
    When building for this target PROJECT_PROCESSOR
    should be set as "hosted".
    
    In this, the "__metal_cpu_yield()" call
    is expected to be provided by the system folder
    headers, to provide whatever is needed for the
    metal_cpu_yield() function to perform its duty.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

---
+++ libmetal/lib/processor/hosted/atomic.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*
+ * @file	hosted/atomic.h
+ * @brief	Hosted environment atomic primitives for libmetal.
+ */
+
+#ifndef __METAL_HOSTED_ATOMIC__H__
+#define __METAL_HOSTED_ATOMIC__H__
+
+#endif /* __METAL_HOSTED_ATOMIC__H__ */
