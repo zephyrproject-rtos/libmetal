14f519529a1e4a46aaea6826f5a41d99a3347276
Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
2025-04-01 14:03:44 +0200

    lib: update libmetal to SHA 9a21915a5f8f
    Origin:
    	https://github.com/OpenAMP/libmetal
    
    Commit:
    	9a21915a5f8f685a9d77672e3817a923d3de8e1f
    
    Status:
    	Update to libmetal main branch that integrate:
    	9a21915a5f8f ("cmake: update cmake minimum support to 3.16")
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>

--- libmetal/lib/system/freertos/io.c
+++ libmetal/lib/system/freertos/io.c
@@ -22,7 +22,7 @@ void metal_sys_io_mem_map(struct metal_io_region *io)
 	if (psize) {
 		if (psize >> io->page_shift)
 			psize = (size_t)1 << io->page_shift;
-		for (p = 0; p <= (io->size >> io->page_shift); p++) {
+		for (p = 0; p <= ((io->size - 1) >> io->page_shift); p++) {
 			metal_machine_io_mem_map(va, io->physmap[p],
 						 psize, io->mem_flags);
 			va += psize;
